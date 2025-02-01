5330d300e960a356e7030e348237d505f6290717
Vikas <vikas1000dhiman@outlook.com>
2025-02-01 21:01:58 +0530

    Add intial fiels and setup dashboard layout

---
+++ app/(dashboard)/add-job/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const AddJob = () => {
+  return (
+    <div>
+      <h2>This is add job page</h2>
+    </div>
+  )
+}
+
+export default AddJob
---
+++ app/(dashboard)/stats/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Stats = () => {
+  return (
+    <div>
+      <h2>This is my stats page</h2>
+    </div>
+  )
+}
+
+export default Stats
